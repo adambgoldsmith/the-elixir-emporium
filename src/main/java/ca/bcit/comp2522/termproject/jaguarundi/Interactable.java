dae84d45482e346dea5ff04f0cba6397e81133f8
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-11-24 16:08:52 -0800

    Create Interactable abstract class

---
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/Interactable.java
@@ -0,0 +1,8 @@
+package ca.bcit.comp2522.termproject.jaguarundi;
+
+public abstract class Interactable {
+    public abstract int getXPosition();
+    public abstract int getYPosition();
+    public abstract int getWidth();
+    public abstract int getHeight();
+}
