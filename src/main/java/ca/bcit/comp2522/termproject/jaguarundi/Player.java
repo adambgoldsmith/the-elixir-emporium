204fd191393f187441dfdc3ce9c4312eb9c650ea
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-11-24 17:02:10 -0800

    Add sprites to player

--- src/main/java/ca/bcit/comp2522/termproject/jaguarundi/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/Player.java
@@ -2,14 +2,19 @@ package ca.bcit.comp2522.termproject.jaguarundi;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
 
-import java.util.ArrayList;
+import java.util.Objects;
 
 public class Player {
+    // TODO: Add all sprites to a map
+    public final static String PLAYER_SPRITE_PATH = "TestSprite.png";
+    public final static String PLAYER_SPRITE_PATH_INGREDIENT = "player_purple_ingredient.png";
+    public final static String PLAYER_SPRITE_PATH_EMPTY_BOTTLE = "player_empty_bottle.png";
+    public final static String PLAYER_SPRITE_PATH_FILLED_BOTTLE= "player_filled_bottle.png";
     public final static int PLAYER_WIDTH = 50;
     public final static int PLAYER_HEIGHT = 50;
     public final static Color PLAYER_COLOR = Color.BLUE;
-    public final static int INVENTORY_SIZE = 1;
 
     private double speed;
     private double xPosition;
@@ -19,8 +24,8 @@ public class Player {
     private double width;
     private double height;
     private Color color;
-    private Bottle bottle;
-    private ArrayList<Ingredient> inventory;
+    private Item inventory;
+    private Image sprite;
 
     public Player(final int speed, final int xPosition, final int yPosition) {
         this.speed = speed;
@@ -29,13 +34,28 @@ public class Player {
         this.width = PLAYER_WIDTH;
         this.height = PLAYER_HEIGHT;
         this.color = PLAYER_COLOR;
-        this.inventory = new ArrayList<Ingredient>();
-        this.bottle = null;
+        this.inventory = null;
+        this.sprite = new Image(Objects.requireNonNull(getClass().getResourceAsStream(PLAYER_SPRITE_PATH)));
     }
 
     public void draw(GraphicsContext gc) {
-        gc.setFill(color);
-        gc.fillRect(xPosition, yPosition, width, height);
+        gc.drawImage(sprite, xPosition, yPosition, width, height);
+    }
+
+    // TODO: rework this method
+    public void animate() {
+        if (this.inventory != null && this.inventory.getClass() == Ingredient.class) {
+            this.sprite = new Image(Objects.requireNonNull(getClass().getResourceAsStream(PLAYER_SPRITE_PATH_INGREDIENT)));
+        } else if (this.inventory != null && this.inventory.getClass() == Bottle.class) {
+            Bottle bottle = (Bottle) this.inventory;
+            if (bottle.getIngredients().isEmpty()) {
+                this.sprite = new Image(Objects.requireNonNull(getClass().getResourceAsStream(PLAYER_SPRITE_PATH_EMPTY_BOTTLE)));
+            } else {
+                this.sprite = new Image(Objects.requireNonNull(getClass().getResourceAsStream(PLAYER_SPRITE_PATH_FILLED_BOTTLE)));
+            }
+        } else {
+            this.sprite = new Image(Objects.requireNonNull(getClass().getResourceAsStream(PLAYER_SPRITE_PATH)));
+        }
     }
 
     public double getXPosition() {
@@ -44,6 +64,14 @@ public class Player {
 
     public double getYPosition() {
         return yPosition;
+    }
+
+    public void setXPosition(double xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public void setYPosition(double yPosition) {
+        this.yPosition = yPosition;
     }
 
     public void setXDirection(int xDirection) {
@@ -62,40 +90,39 @@ public class Player {
         return height;
     }
 
-    public ArrayList<Ingredient> getInventory() {
+    public Item getInventory() {
         return inventory;
     }
 
-    public void move(double elapsedTime) {
+    public void move(double delta) {
         if (xDirection == 1) {
-            xPosition += speed * elapsedTime;
+            xPosition += speed * delta;
         } else if (xDirection == -1) {
-            xPosition -= speed * elapsedTime;
+            xPosition -= speed * delta;
         }
         if (yDirection == 1) {
-            yPosition += speed * elapsedTime;
+            yPosition += speed * delta;
         } else if (yDirection == -1) {
-            yPosition -= speed * elapsedTime;
+            yPosition -= speed * delta;
         }
     }
 
-    public void addIngredientToInventory(final Ingredient ingredient) {
-        if (this.inventory.size() < INVENTORY_SIZE) {
-            this.inventory.add(ingredient);
+    public void pickUpIngredient(final Ingredient ingredient) {
+        if (this.inventory == null) {
+            this.inventory = ingredient;
+            System.out.println("Player picked up an ingredient!");
         }
     }
 
     public void pickUpBottle(final Bottle bottle) {
-        if (this.bottle == null) {
-            this.bottle = bottle;
+        if (this.inventory == null) {
+            this.inventory = bottle;
             System.out.println("Player picked up a bottle!");
         }
     }
 
-    public void fillBottle(final Ingredient firsIngredient, final Ingredient secondIngredient) {
-        if (this.bottle != null) {
-            this.bottle.fill(firsIngredient, secondIngredient);
-        }
+    public void removeFromInventory() {
+        this.inventory = null;
     }
 
     @Override
@@ -107,7 +134,6 @@ public class Player {
                 ", width=" + width +
                 ", height=" + height +
                 ", color=" + color +
-                ", bottle=" + bottle +
                 ", inventory=" + inventory +
                 '}';
     }
