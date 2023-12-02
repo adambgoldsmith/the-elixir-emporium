a9dd65205a1f6a823cc49571d80c79a04a6ae059
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-12-01 20:37:55 -0800

    Update Collidable to use doubles

--- src/main/java/ca/bcit/comp2522/termproject/jaguarundi/Collidable.java
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/Collidable.java
@@ -1,8 +1,8 @@
 package ca.bcit.comp2522.termproject.jaguarundi;
 
 public interface Collidable {
-    int getXPosition();
-    int getYPosition();
-    int getWidth();
-    int getHeight();
+    double getXPosition();
+    double getYPosition();
+    double getWidth();
+    double getHeight();
 }
