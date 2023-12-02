677ca5f44152ff0906d8f7fa74a30e56ab5bcff2
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-12-01 20:42:07 -0800

    Update wall sprites

--- src/main/java/ca/bcit/comp2522/termproject/jaguarundi/Wall.java
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/Wall.java
@@ -1,43 +1,83 @@
 package ca.bcit.comp2522.termproject.jaguarundi;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Wall implements Collidable {
-    public final static Color WALL_COLOR = Color.DARKRED;
+    private static final Map<String, Image> WALL_SPRITE_MAP = new HashMap<>();
+    static {
+        WALL_SPRITE_MAP.put("top", new Image(Objects.requireNonNull(Wall.class.getResourceAsStream("wall_top.png"))));
+        WALL_SPRITE_MAP.put("bottom", new Image(Objects.requireNonNull(Wall.class.getResourceAsStream("wall_bottom.png"))));
+        WALL_SPRITE_MAP.put("side", new Image(Objects.requireNonNull(Wall.class.getResourceAsStream("wall_side.png"))));
+        WALL_SPRITE_MAP.put("counter", new Image(Objects.requireNonNull(Wall.class.getResourceAsStream("counter.png"))));
+    }
 
-    private final int xPosition;
-    private final int yPosition;
-    private final int width;
-    private final int height;
-    private final Color color;
+    private double xPosition;
+    private double yPosition;
+    private final double width;
+    private final double height;
+    private final String wallType;
 
-    public Wall(final int xPosition, final int yPosition, final int width, final int height) {
-        this.xPosition = xPosition;
-        this.yPosition = yPosition;
+    public Wall(final int width, final int height, final String wallType) {
+        this.xPosition = 0;
+        this.yPosition = 0;
         this.width = width;
         this.height = height;
-        this.color = WALL_COLOR;
+        this.wallType = wallType;
     }
 
     public void draw(GraphicsContext gc) {
-        gc.setFill(color);
-        gc.fillRect(xPosition, yPosition, width, height);
+        switch (this.wallType) {
+            case "top" -> {
+                for (int i = 0; i < width; i += 50) {
+                    gc.drawImage(WALL_SPRITE_MAP.get("top"), xPosition + i, yPosition);
+                }
+            }
+            case "bottom" -> {
+                for (int i = 0; i < width; i += 50) {
+                    gc.drawImage(WALL_SPRITE_MAP.get("bottom"), xPosition + i, yPosition);
+                }
+            }
+            case "side" -> {
+                for (int i = 0; i < height; i += 50) {
+                    gc.drawImage(WALL_SPRITE_MAP.get("side"), xPosition, yPosition + i);
+                }
+            }
+            case "counter" -> {
+                for (int i = 0; i < height; i += 50) {
+                    gc.drawImage(WALL_SPRITE_MAP.get("counter"), xPosition, yPosition + i);
+                }
+            }
+        }
+
     }
 
-    public int getXPosition() {
+    public double getXPosition() {
         return xPosition;
     }
 
-    public int getYPosition() {
+    public double getYPosition() {
         return yPosition;
     }
 
-    public int getWidth() {
+    public void setXPosition(double xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public void setYPosition(double yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public double getWidth() {
         return width;
     }
 
-    public int getHeight() {
+    public double getHeight() {
         return height;
     }
 }
