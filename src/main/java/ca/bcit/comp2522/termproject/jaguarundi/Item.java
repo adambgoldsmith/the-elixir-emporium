a9afc64386674eb4022247e47e9fde5d6aee3db2
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-12-01 20:40:45 -0800

    Adjust Item parent class to be more polymorphic

--- src/main/java/ca/bcit/comp2522/termproject/jaguarundi/Item.java
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/Item.java
@@ -1,13 +1,6 @@
 package ca.bcit.comp2522.termproject.jaguarundi;
 
 public abstract class Item {
-    private final String name;
-
-    public Item(final String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
+    public Item() {
     }
 }
