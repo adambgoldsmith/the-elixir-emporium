156743c15907b3889b24719b105feb7ef0e769ac
VIV\vivian <mchu65@my.bcit.ca>
2023-12-02 19:18:55 -0800

    Modify SaveLoadDialog and TitleScreen for the player to save their profile

--- src/main/java/ca/bcit/comp2522/termproject/jaguarundi/SaveLoadDialog.java
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/SaveLoadDialog.java
@@ -6,6 +6,7 @@ import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class SaveLoadDialog {
--- src/main/java/ca/bcit/comp2522/termproject/jaguarundi/TitleScreen.java
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/TitleScreen.java
@@ -15,9 +15,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Optional;
+import java.util.*;
 
 public class TitleScreen {
 
