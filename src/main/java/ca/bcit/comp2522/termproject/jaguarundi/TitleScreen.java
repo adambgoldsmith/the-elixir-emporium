4f4d85fd67bcc1c78b2592810f6442c6a77982b5
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-12-05 11:06:43 -0800

    Clean up TitleScreen class

--- src/main/java/ca/bcit/comp2522/termproject/jaguarundi/TitleScreen.java
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/TitleScreen.java
@@ -6,10 +6,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.control.TextInputDialog;
-
 import java.io.*;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -20,18 +17,19 @@ import java.util.*;
 
 public class TitleScreen {
     public final static Image TITLE_SCREEN_ART = new Image(Objects.requireNonNull(TitleScreen.class.getResourceAsStream("TitleScreenArt.png")));
+    public final static double BUTTON_WIDTH = 300;
+    public final static double BUTTON_HEIGHT = 75;
+    private static final String NAME_LEVEL_REGEX = "\\*.*\\*\\d+";
 
     private GameApp gameApp;
     private GameManager gameManager;
-    private double buttonWidth = 300;
-    private double buttonHeight = 75;
 
     public TitleScreen(GameApp gameApp, GameManager gameManager) {
         this.gameApp = gameApp;
         this.gameManager = gameManager;
     }
 
-    public void draw(GraphicsContext gc) {
+    public void draw(final GraphicsContext gc) {
         // Draw the background image
         gc.drawImage(TITLE_SCREEN_ART, 0, 0, 1000, 550);
 
@@ -39,134 +37,114 @@ public class TitleScreen {
         gc.setFill(Color.BLACK);
         gc.setFont(Font.font("Arial", FontWeight.BOLD, 40));
 
-
         // Start button
         gc.setFill(Color.MEDIUMPURPLE);
-        gc.fillRect(100, 250, buttonWidth, buttonHeight);
+        gc.fillRect(100, 250, BUTTON_WIDTH, BUTTON_HEIGHT);
         gc.setFill(Color.WHITE);
-        gc.fillText("Start", 140 + buttonWidth / 2 - gc.getFont().getSize() * 2, 265 + buttonHeight / 2);
+        gc.fillText("Start", 140 + BUTTON_WIDTH / 2 - gc.getFont().getSize() * 2, 265 + BUTTON_HEIGHT / 2);
 
         // Quit button
         gc.setFill(Color.MEDIUMPURPLE);
-        gc.fillRect(100, 350, buttonWidth, buttonHeight);
+        gc.fillRect(100, 350, BUTTON_WIDTH, BUTTON_HEIGHT);
         gc.setFill(Color.WHITE);
-        gc.fillText("Quit", 140 + buttonWidth / 2 - gc.getFont().getSize() * 2, 365 + buttonHeight / 2);
+        gc.fillText("Quit", 140 + BUTTON_WIDTH / 2 - gc.getFont().getSize() * 2, 365 + BUTTON_HEIGHT / 2);
     }
 
-    public void update(MouseEvent mouseEvent) {
+    public void update(final MouseEvent mouseEvent) {
         double mouseX = mouseEvent.getX();
         double mouseY = mouseEvent.getY();
 
-        // Check if the mouse is within the bounds of the Start button
-        if (isMouseInButton(mouseX, mouseY, 100, 250, buttonWidth, buttonHeight)) {
-            // Invoke Start button's method
+        if (isMouseInButton(mouseX, mouseY, 100, 250, BUTTON_WIDTH, BUTTON_HEIGHT)) {
             handleStartButtonClick();
         }
 
-        // Check if the mouse is within the bounds of the Quit button
-        else if (isMouseInButton(mouseX, mouseY, 100, 350, buttonWidth, buttonHeight)) {
-            // Invoke Quit button's method
+        else if (isMouseInButton(mouseX, mouseY, 100, 350, BUTTON_WIDTH, BUTTON_HEIGHT)) {
             handleQuitButtonClick();
         }
     }
 
-    private boolean isMouseInButton(double mouseX, double mouseY, double x, double y, double width, double height) {
-        return mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height;
+    private boolean isMouseInButton(final double mouseX, final double mouseY, final double buttonX, final double buttonY, final double buttonWidth, final double buttonHeight) {
+        return mouseX >= buttonX && mouseX <= buttonX + buttonWidth && mouseY >= buttonY && mouseY <= buttonY + buttonHeight;
     }
 
     private void handleStartButtonClick() {
         Map<String, Integer> namesAndNumbers = readNamesFromFile("saves.txt");
         Optional<String> result = SaveLoadDialog.showSaveLoadDialog(namesAndNumbers);
 
-        // If the user entered a name, process it
-        result.ifPresent(name -> {
-            System.out.println("User entered name: " + name);
+        result.ifPresent(this::processUserInput);
+    }
 
-            // Check if the name is in the format *name*level
-            if (name.matches("\\*.*\\*\\d+")) {
-                String[] parts = name.split("\\*");
-                String enteredName = parts[1];
-                int level = Integer.parseInt(parts[2]);
-                System.out.println(Arrays.toString(parts));
+    private void processUserInput(final String name) {
+        if (name.matches(NAME_LEVEL_REGEX)) {
+            handleNamedLevelInput(name);
+        } else {
+            handleRegularNameInput(name);
+        }
 
-                // Add logic to handle the entered name and level
-                // (You might want to store the name and level in your game data)
+        gameApp.setGameStarted(true);
+    }
 
-                gameManager.setCurrentLevelIndex(level-1);
-                if (!name.isEmpty()) {
-                    gameManager.setCurrentUser(enteredName);
-                    System.out.println("Current user: " + enteredName);
-                }
-            } else {
-                // If the name is just a name with no *, append it to the file
-                if (!name.isEmpty()) {
-                    appendNameToFile(name, "saves.txt");
-                }
-                // Set the level index to 0
-                gameManager.setCurrentLevelIndex(0);
-                if (!name.isEmpty()) {
-                    gameManager.setCurrentUser(name);
-                    System.out.println("Current user: " + name);
-                }
+    private void handleNamedLevelInput(final String name) {
+        String[] nameParts = name.split("\\*");
+
+        if (nameParts.length == 3) {
+            String enteredName = nameParts[1];
+            int level = Integer.parseInt(nameParts[2]);
+
+            gameManager.setCurrentLevelIndex(level - 1);
+
+            if (!enteredName.isEmpty()) {
+                gameManager.setCurrentUser(enteredName);
+                System.out.println("Current user: " + enteredName);
             }
+        }
+    }
 
-            // Set game started to true in both cases
-            gameApp.setGameStarted(true);
-        });
+    private void handleRegularNameInput(final String name) {
+        if (!name.isEmpty()) {
+            appendNameToFile(name, "saves.txt");
+        }
+
+        gameManager.setCurrentLevelIndex(0);
+
+        if (!name.isEmpty()) {
+            gameManager.setCurrentUser(name);
+            System.out.println("Current user: " + name);
+        }
     }
 
     private void handleQuitButtonClick() {
-        // Add logic for Quit button click
+        // TODO: Add quit button functionality
         System.out.println("Quit button clicked");
     }
 
-    private Map<String, Integer> readNamesFromFile(String fileName) {
+    private Map<String, Integer> readNamesFromFile(final String fileName) {
         Map<String, Integer> namesAndNumbers = new HashMap<>();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(
                 TitleScreen.class.getResourceAsStream("/ca/bcit/comp2522/termproject/jaguarundi/" + fileName), StandardCharsets.UTF_8))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Skip empty lines
-                if (!line.trim().isEmpty()) {
-                    // Split the line into name and number using '=' as the delimiter
-                    String[] parts = line.split("=");
-                    if (parts.length == 2) {
-                        String name = parts[0].trim();
-                        int number = Integer.parseInt(parts[1].trim());
-                        namesAndNumbers.put(name, number);
-                    } else {
-                        // Handle lines that do not follow the expected format
-                        System.err.println("Invalid line format: " + line);
-                    }
-                }
-            }
+            reader.lines()
+                    .filter(line -> !line.trim().isEmpty())
+                    .forEach(line -> {
+                        String[] parts = line.split("=");
+                        if (parts.length == 2) {
+                            namesAndNumbers.put(parts[0].trim(), Integer.parseInt(parts[1].trim()));
+                        } else {
+                            System.err.println("Invalid line format: " + line);
+                        }
+                    });
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         return namesAndNumbers;
     }
 
-
-
-    private void appendNameToFile(String name, String fileName) {
+    private void appendNameToFile(final String name, final String fileName) {
         try {
-            // Get the file path
-            URI uri = TitleScreen.class.getResource("/ca/bcit/comp2522/termproject/jaguarundi/" + fileName).toURI();
-            Path path = Paths.get(uri);
-
-            // Append the entered name as a new line, including a line separator
-            Files.write(
-                    path,
-                    (name + "=1" + System.lineSeparator()).getBytes(StandardCharsets.UTF_8),
-                    StandardOpenOption.CREATE,  // Create the file if it does not exist
-                    StandardOpenOption.APPEND   // Append to the file
-            );
+            Path path = Paths.get(TitleScreen.class.getResource("/ca/bcit/comp2522/termproject/jaguarundi/" + fileName).toURI());
+            Files.write(path, (name + "=1" + System.lineSeparator()).getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
         } catch (IOException | URISyntaxException e) {
-            // Handle the exception appropriately, e.g., log it or throw a custom exception
             e.printStackTrace();
         }
     }
-
-
-
 }
