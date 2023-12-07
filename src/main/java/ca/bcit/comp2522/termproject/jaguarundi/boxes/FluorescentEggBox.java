ecd42963d62377ff55d9d4ee02ae79cdd3d7e82f
VIV\vivian <mchu65@my.bcit.ca>
2023-12-06 21:01:38 -0800

    Add javadoc for FluorescentEggBox.java

--- src/main/java/ca/bcit/comp2522/termproject/jaguarundi/boxes/FluorescentEggBox.java
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/boxes/FluorescentEggBox.java
@@ -4,10 +4,22 @@ import ca.bcit.comp2522.termproject.jaguarundi.holdables.FluorescentEgg;
 import javafx.scene.image.Image;
 
 import java.util.Objects;
+/**
+ * A box that holds fluorescent eggs.
+ *
+ * @author Vivian , Adam
+ * @version 2023
+ */
 
 public class FluorescentEggBox extends IngredientBox {
-    public final static Image FLUORESCENT_EGG_BOX_SPRITE = new Image(Objects.requireNonNull(FluorescentEggBox.class.getResourceAsStream("fluorescent_egg_crate.png")));
-
+    /**
+     * The sprite for the fluorescent egg box.
+     */
+    public final static Image FLUORESCENT_EGG_BOX_SPRITE = new Image(Objects.requireNonNull(FluorescentEggBox.class.
+            getResourceAsStream("fluorescent_egg_crate.png")));
+    /**
+     * Constructs a fluorescent egg box.
+     */
     public FluorescentEggBox() {
         super(new FluorescentEgg(), FLUORESCENT_EGG_BOX_SPRITE);
     }
