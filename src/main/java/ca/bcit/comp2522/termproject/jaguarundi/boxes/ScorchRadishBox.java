543fc3cc29e3c9faecbff822c3344e49fc70781e
VIV\vivian <mchu65@my.bcit.ca>
2023-12-06 21:07:45 -0800

    Add javadoc for ScorchRadishBox.java

--- src/main/java/ca/bcit/comp2522/termproject/jaguarundi/boxes/ScorchRadishBox.java
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/boxes/ScorchRadishBox.java
@@ -4,9 +4,23 @@ import ca.bcit.comp2522.termproject.jaguarundi.holdables.ScorchRadish;
 import javafx.scene.image.Image;
 
 import java.util.Objects;
+/**
+ * A box that holds scorch radishes.
+ *
+ * @author Vivian , Adam
+ * @version 2023
+ */
 
 public class ScorchRadishBox extends IngredientBox {
-    public final static Image SCORCHRADISH_BOX_SPRITE = new Image(Objects.requireNonNull(ScorchRadishBox.class.getResourceAsStream("scorch_radish_crate.png")));
+    /**
+     * The sprite for the scorch radish box.
+     */
+    public final static Image SCORCHRADISH_BOX_SPRITE = new Image(Objects.requireNonNull(ScorchRadishBox.class.
+            getResourceAsStream("scorch_radish_crate.png")));
+
+    /**
+     * Constructs a scorch radish box.
+     */
 
     public ScorchRadishBox() {
         super(new ScorchRadish(), SCORCHRADISH_BOX_SPRITE);
