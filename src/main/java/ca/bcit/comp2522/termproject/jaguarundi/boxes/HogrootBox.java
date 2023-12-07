a84f4b2cb521d1a13ef1aab3015a9303f5745323
VIV\vivian <mchu65@my.bcit.ca>
2023-12-06 21:02:58 -0800

    Add javadoc for HogrootBox.java

--- src/main/java/ca/bcit/comp2522/termproject/jaguarundi/boxes/HogrootBox.java
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/boxes/HogrootBox.java
@@ -3,10 +3,22 @@ package ca.bcit.comp2522.termproject.jaguarundi.boxes;
 import ca.bcit.comp2522.termproject.jaguarundi.holdables.Hogroot;
 import javafx.scene.image.Image;
 import java.util.Objects;
-
+/**
+ * A box that holds hogroot.
+ *
+ * @author Vivian , Adam
+ * @version 2023
+ */
 public class HogrootBox extends IngredientBox {
-    public final static Image HOGROOT_BOX_SPRITE = new Image(Objects.requireNonNull(HogrootBox.class.getResourceAsStream("hogroot_crate.png")));
+    /**
+     * The sprite for the hogroot box.
+     */
+    public final static Image HOGROOT_BOX_SPRITE = new Image(Objects.requireNonNull(HogrootBox.class.
+            getResourceAsStream("hogroot_crate.png")));
 
+    /**
+     * Constructs a hogroot box.
+     */
     public HogrootBox() {
         super(new Hogroot(), HOGROOT_BOX_SPRITE);
     }
