d65f5e23124e8923eb113288bf992746ef8f1627
VIV\vivian <mchu65@my.bcit.ca>
2023-12-06 21:00:04 -0800

    Modify javadoc for CobaltCompoundBox.java

--- src/main/java/ca/bcit/comp2522/termproject/jaguarundi/boxes/CobaltCompoundBox.java
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/boxes/CobaltCompoundBox.java
@@ -4,10 +4,25 @@ import ca.bcit.comp2522.termproject.jaguarundi.holdables.CobaltCompound;
 import javafx.scene.image.Image;
 
 import java.util.Objects;
+/**
+ * A box that holds cobalt compound.
+ *
+ * @author Vivian , Adam
+ * @version 2023
+ *
+ */
+
 
 public class CobaltCompoundBox extends IngredientBox {
-    public final static Image COBALT_COMPOUND_BOX_SPRITE = new Image(Objects.requireNonNull(CobaltCompoundBox.class.getResourceAsStream("cobalt_compound_crate.png")));
+    /**
+     * The sprite for the cobalt compound box.
+     */
+    public final static Image COBALT_COMPOUND_BOX_SPRITE = new Image(Objects.requireNonNull(CobaltCompoundBox.class.
+            getResourceAsStream("cobalt_compound_crate.png")));
 
+    /**
+     * Constructs a cobalt compound box.
+     */
     public CobaltCompoundBox() {
         super(new CobaltCompound(), COBALT_COMPOUND_BOX_SPRITE);
     }
