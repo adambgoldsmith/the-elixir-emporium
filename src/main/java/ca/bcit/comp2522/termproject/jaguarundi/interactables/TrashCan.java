b6697782e50892f0fe1b57f6d988f5c521ac402b
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-12-06 20:40:17 -0800

    Edit trashcan class

--- src/main/java/ca/bcit/comp2522/termproject/jaguarundi/interactables/TrashCan.java
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/interactables/TrashCan.java
@@ -7,7 +7,9 @@ import javafx.scene.image.Image;
 
 import java.util.Objects;
 
-public class TrashCan extends Interactable implements Collidable {
+public class TrashCan extends Interactable {
+    public final static Image TRASHCAN_SPRITE = new Image(Objects.requireNonNull(TrashCan.class.getResourceAsStream("TrashCan.png")));
+
     public static final int TRASH_BOX_WIDTH = 50;
     public static final int TRASH_BOX_HEIGHT = 50;
 
@@ -15,7 +17,7 @@ public class TrashCan extends Interactable implements Collidable {
     private double yPosition;
     private final double width;
     private final double height;
-    public static final Color BOTTLE_BOX_COLOR = Color.HOTPINK;
+
 
 
     public TrashCan(final double xPosition, final double yPosition) {
