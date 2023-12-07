1de7d20a181d8972cd7fafe52608256125ee09b5
VIV\vivian <mchu65@my.bcit.ca>
2023-12-06 20:57:25 -0800

    Add javadoc for BottleBox.java

--- src/main/java/ca/bcit/comp2522/termproject/jaguarundi/boxes/BottleBox.java
+++ src/main/java/ca/bcit/comp2522/termproject/jaguarundi/boxes/BottleBox.java
@@ -6,16 +6,39 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
 import java.util.Objects;
+/**
+ * A box that holds bottles.
+ *
+ * @author Vivian ,Adam
+ * @version 2023
+ *     The type of elements in this set.
+ */
 
 public class BottleBox extends Interactable implements Collidable {
-    public final static Image BOTTLE_BOX_SPRITE = new Image(Objects.requireNonNull(BottleBox.class.getResourceAsStream("bottle_table.png")));
+    /**
+     * The sprite for the bottle box.
+     */
+    public final static Image BOTTLE_BOX_SPRITE = new Image(Objects.requireNonNull(BottleBox.class.
+            getResourceAsStream("bottle_table.png")));
+    /**
+     * The width of the bottle box.
+     */
     public static final int BOTTLE_BOX_WIDTH = 50;
+    /**
+     * The height of the bottle box.
+     */
     public static final int BOTTLE_BOX_HEIGHT = 50;
 
     private final double xPosition;
     private final double yPosition;
     private final double width;
     private final double height;
+    /**
+     * Constructs a bottle box.
+     *
+     * @param xPosition the x position of the bottle box
+     * @param yPosition the y position of the bottle box
+     */
 
     public BottleBox(final int xPosition, final int yPosition) {
         this.xPosition = xPosition;
@@ -23,22 +46,47 @@ public class BottleBox extends Interactable implements Collidable {
         this.width = BOTTLE_BOX_WIDTH;
         this.height = BOTTLE_BOX_HEIGHT;
     }
+    /**
+     * Draws the bottle box.
+     *
+     * @param gc the graphics context to draw with
+     */
 
     public void draw(final GraphicsContext gc) {
         gc.drawImage(BOTTLE_BOX_SPRITE, xPosition, yPosition);
     }
+    /**
+     * Gets the x position of the bottle box.
+     *
+     * @return the x position of the bottle box
+     */
 
     public double getXPosition() {
         return xPosition;
     }
+    /**
+     * Gets the y position of the bottle box.
+     *
+     * @return the y position of the bottle box
+     */
 
     public double getYPosition() {
         return yPosition;
     }
+    /**
+     * Gets the width of the bottle box.
+     *
+     * @return the width of the bottle box
+     */
 
     public double getWidth() {
         return width;
     }
+    /**
+     * Gets the height of the bottle box.
+     *
+     * @return the height of the bottle box
+     */
 
     public double getHeight() {
         return height;
